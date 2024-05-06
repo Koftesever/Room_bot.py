b0a13fd34cff907fc4a078c6cc52ae82fc732600
Koftesever <147646974+Koftesever@users.noreply.github.com>
2024-05-06 22:14:59 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+from room_checker import *
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def room_check(ctx):
+    await ctx.send("Odanın fotografını yolla!")
+
+bot.run("token")
